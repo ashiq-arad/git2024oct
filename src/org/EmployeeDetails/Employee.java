2a91f4bf222ab070750a0c5264cdca66272b7e92
ashiq-arad <Ashiqbs65@yahoo.com>
2024-10-10 02:34:27 +0530

    testone

---
+++ src/org/EmployeeDetails/Employee.java
@@ -0,0 +1,21 @@
+package org.EmployeeDetails;
+
+public class Employee {
+	
+	public void empName() {
+		System.out.println("github");
+		
+    }
+	public void empNo() {
+		System.out.println("12345");
+		
+    }
+	
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		e.empName();
+		e.empNo();
+	}
+
+}
